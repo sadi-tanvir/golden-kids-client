d77c712c3195888dc46b5581e25e6e2a761f81f1
rafin8990 <rafin8990@gmail.com>
2024-01-06 15:23:10 +0600

    code edited

--- src/app/(withLayout)/layout.tsx
+++ src/app/(withLayout)/layout.tsx
@@ -1,11 +1,14 @@
+import Footer from "../shared/Footer";
 import Navbar from "../shared/Navbar";
 
 
 const MainLayout = ({children}:{ children: React.ReactNode }) => {
     return (
         <div>
+
             <Navbar></Navbar>
             {children}
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+
+
+const NotfoundPage = () => {
+    return (
+        <div>
+            <h1>404!!! Page Not found</h1>
+        </div>
+    );
+};
+
+export default NotfoundPage;
